2bcbeef2f48521ac1783fe7369679b5fddf358fe
Bernardo Binda <bbinda@guideposts.org>
2019-11-19 20:51:19 -0500

    added song lyric in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Faded again, but I won't give in
+          MACHINES DON'T BLEED!
         </p>
         <a
           className="App-link"
